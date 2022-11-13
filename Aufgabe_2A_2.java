66d77b8c1ded7021efdea58eaabf428ee62e79dc
tabs <67957760+AeonGenesis@users.noreply.github.com>
2022-11-13 02:27:50 +0100

    funktioniert

--- Aufgabe_2A_2.java
+++ Aufgabe_2A_2.java
@@ -1,57 +1,29 @@
 import java.io.*;
 
-/*
- * public static void main (String[] args)
-{
-    int n = 4;
-    String printFormat = "%"+n+"s";
-    int max = (int) Math.pow(2, n);
-    for (int i=0; i < max; i++) {
-      String s = String.format(printFormat, Integer.toString(i, 2)).replace(' ', '0');  
-      System.out.println(s);
-    }
-}
-
- */
 public class Aufgabe_2A_2{
 
 
     public static void main(String[] args) throws IOException {
         //Var init
-        String s = "start";
-        char c = 'A';
+        int n;
         BufferedReader eingabe = new BufferedReader(new InputStreamReader(System.in));
-
         
         //korrekte Eingabe prüfen
         do {
-            System.out.println("Bitte String kürzer als 8 eingeben: ");
-            s = new String(eingabe.readLine());
-            System.out.println("Gelesener String: " + s);
-            System.out.println("s.length(): " + s.length());
-        }while (s.length() >= 8);
+            System.out.println("Bitte Granzzahl n groesser als 1 eingeben: ");
+            n = Integer.parseInt(eingabe.readLine());
+            System.out.println("eingegebene zahl: " + n);
+        }while (n < 1);
 
-
+        int out = n;
         //Char Eingabe
-        System.out.println("Bitte char-Wert eingeben: ");
-        c = (char) eingabe.read();
-        eingabe.readLine(); // wichtig: entfernt das Enter-Zeichen aus dem Eingabepuffer
-        System.out.println("Gelesener char-Wert: " + c);
-        System.out.println();
-
-        int z = 0;
+        //int max = (int) Math.pow(2, n);
         int i;
-        System.out.println(s.indexOf(c)+1);
-        for (i=0; i < s.length()-1; i++){
-            if (i == s.indexOf(c)) {
-                z = z++;
-            } 
-            else {
-            
-            }
-        
+        for (i=0; n > 0; i++){
+            n = n/2;
         }
-        System.out.println(z);            
+        System.out.println(i);  
+        System.out.println(out+ " in binaer ist " + Integer.toBinaryString(out));       
     }
 
 }
